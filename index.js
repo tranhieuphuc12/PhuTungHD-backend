4008520471d2afd7d1b9b69c3b796d0565639d5c
PhucTran <tranhieuphuc12@gmail.com>
2025-05-11 14:12:35 +0700

    remove cors

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const PORT = process.env.PORT_BACKEND_HD || 5000;
 connectDB();
 const app = express();
 // Middleware
-app.use(cors
-  ({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  }
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
